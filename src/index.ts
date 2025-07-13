4c1f3d72956b1694b4e500ddd84d02b9fd11c7a4
Digital39999 <github@crni.xyz>
2025-07-13 23:25:20 +0200

    Add default export for core modules in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import * as Manager from './core/manager';
+import * as Utils from './core/utils';
+import * as Types from './core/types';
+import * as Bot from './core/bot';
+
 export * from './core/manager';
 export * from './core/types';
 export * from './core/utils';
 export * from './core/bot';
+
+export default {
+	Manager,
+	Types,
+	Utils,
+	Bot,
+};
